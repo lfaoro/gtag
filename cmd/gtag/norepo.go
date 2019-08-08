60e422da22b03679d0b681741a009e1987749715
Leonardo <lfaoro@gmail.com>
2019-08-08 13:41:29 +0200

    general refactor
    Signed-off-by: Leonardo <lfaoro@gmail.com>

--- cmd/gtag/norepo.go
+++ cmd/gtag/norepo.go
@@ -1,20 +1,2 @@
 package main
 
-import (
-	"github.com/pkg/errors"
-	"os/exec"
-	"strings"
-)
-
-func isGitRepo() error {
-	out, err := exec.Command("sh", "-c",
-		"git --no-pager tag -l").CombinedOutput()
-	if err != nil {
-		yes := strings.Contains(string(out), "not a git repository")
-		if yes {
-			return errors.Wrap(err, "not a git repository")
-		}
-		return err
-	}
-	return nil
-}
--- cmd/gtag/shellCmd.go
+++ cmd/gtag/shellCmd.go
@@ -1,12 +1,29 @@
 package main
 
-import "os/exec"
+import (
+	"os/exec"
+	"strings"
+
+	"github.com/pkg/errors"
+)
 
 func shellCmd(cmd string) (output string, err error) {
 	out, err := exec.Command("sh", "-c",
 		cmd).CombinedOutput()
 	if err != nil {
-		return "", err
+		return "", errors.Wrap(err, string(out))
 	}
 	return string(out), nil
+}
+
+func isGitRepo() error {
+	out, err := shellCmd("git --no-pager tag -l")
+	if err != nil {
+		yes := strings.Contains(out, "not a git repository")
+		if yes {
+			return errors.Wrap(err, "not a git repository")
+		}
+		return errors.Wrap(err, out)
+	}
+	return nil
 }
