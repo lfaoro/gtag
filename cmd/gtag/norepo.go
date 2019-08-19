8a207130b3985d66a967de44dd4870a8f341e1c0
Leonardo <lfaoro@gmail.com>
2019-08-19 12:31:24 +0200

    add copyright statement to all go files

--- cmd/gtag/norepo.go
+++ cmd/gtag/norepo.go
@@ -1,2 +1,5 @@
-package main
+// Copyright (c) 2019 Leonardo Faoro. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
+package main
